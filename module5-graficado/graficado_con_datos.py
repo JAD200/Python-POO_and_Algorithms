7ff47c036fd27747175756fa05a243256c066c3e
Juan Di Pasquo <juandipasquo@gmail.com>
2022-04-22 23:06:00 -0300

    Converted the csv in to a dictionary & showed the data

--- module5-graficado/graficado_con_datos.py
+++ module5-graficado/graficado_con_datos.py
@@ -2,49 +2,50 @@
 from bokeh.plotting import figure, output_file, show, save, ColumnDataSource
 from bokeh.models.tools import HoverTool
 from bokeh.transform import factor_cmap
-from bokeh.palettes import Magma256
+from bokeh.palettes import Viridis256
 # Pandas
-import pandas
+# import pandas
 
+from data import provinces, dates, data
 
 if __name__ == '__main__':
 
 #*   CSV
     # Read in csv
-    data_frame = pandas.read_csv('w_mean_tests.csv')
+    # data_frame = pandas.read_csv('w_mean_tests.csv')
 
     # Create ColumnDataSource from data frame
-    source = ColumnDataSource(data_frame)
+    # source = ColumnDataSource(data_frame)
 
     output_file('women_salary.html')
 
-    # Car list
-    year = source.data['fecha'].tolist()
+    # date list
+    # date = source.data['fecha'].tolist()
 
 #*   Add plot
     graph = figure(
-        x_range=year,
-        plot_width=700,
-        plot_height=600,
+        x_range=provinces,
+        plot_height=800,
+        plot_width=900,
         title='Women salary',
         title_location='left',
         x_axis_label='Salaries',
         y_axis_label='Date',
-        toolbar_location='above',
+        toolbar_location='above'
     )
 
 #*   Render glyph
     graph.vbar_stack(
-        'w_mean',
-        x='fecha',
+        dates,
+        x='provinces',
         width=0.9,
         fill_color=factor_cmap(
-            'fecha',
-            palette=Magma256,
-            factors=year
+            'provinces',
+            palette=Viridis256,
+            factors=provinces
         ),
         fill_alpha=0.9,
-        source=source
+        source=data
     )
 
     graph.y_range.start = 0
@@ -53,18 +54,13 @@ if __name__ == '__main__':
     graph.axis.minor_tick_line_color = None
     graph.outline_line_color = None
 
-    # Add Legend
-    # graph.legend.orientation = 'vertical'
-    # graph.legend.location = 'top_right'
-    # graph.legend.label_text_font_size = '10px'
-
-    # Add Tooltips
+    # # Add Tooltips
     hover = HoverTool()
     hover.tooltips = """
         <div>
-            <h3>@zona_prov</h3>
-            <div><b>Fecha</b> @fecha</div>
-            <div><b>Salarie</b> @w_mean</div>
+            <h3>$name</h3>
+            <div><b>Provincia</b> @provinces</div>
+            <div><b>Salarie</b> @$dates</div>
         </div>
     """
     graph.add_tools(hover)
