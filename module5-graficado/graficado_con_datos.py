2ea54d69e24ceaae64713df6e86a79679f039505
Juan Di Pasquo <juandipasquo@gmail.com>
2022-04-24 14:18:46 -0300

    Improved the color palette

--- module5-graficado/graficado_con_datos.py
+++ module5-graficado/graficado_con_datos.py
@@ -2,20 +2,28 @@
 from bokeh.plotting import figure, output_file, show, save, ColumnDataSource
 from bokeh.models.tools import HoverTool
 from bokeh.transform import factor_cmap
-from bokeh.palettes import Viridis256
+from bokeh.palettes import Purples
 # Pandas
 # import pandas
-
+# Data
 from data import provinces, dates, data
 
+#   Todo
+##  Show a legend with all the dates
+##  Erease the metrics of x axis and improve the annotations
+##  Show a hover with detailed information
+
+
 if __name__ == '__main__':
+
 
 #*   CSV
     # Read in csv
     # data_frame = pandas.read_csv('w_mean_tests.csv')
+    data_frame = data
 
     # Create ColumnDataSource from data frame
-    # source = ColumnDataSource(data_frame)
+    source = ColumnDataSource(data_frame)
 
     output_file('women_salary.html')
 
@@ -25,13 +33,15 @@ if __name__ == '__main__':
 #*   Add plot
     graph = figure(
         x_range=provinces,
-        plot_height=800,
+        plot_height=650,
         plot_width=900,
         title='Women salary',
-        title_location='left',
-        x_axis_label='Salaries',
-        y_axis_label='Date',
-        toolbar_location='above'
+        title_location='above',
+        x_axis_label='Provinces',
+        y_axis_label='Salaries',
+        background_fill_color='#3A3845',
+        toolbar_location='above',
+        tools='pan,wheel_zoom,save,reset',
     )
 
 #*   Render glyph
@@ -39,13 +49,9 @@ if __name__ == '__main__':
         dates,
         x='provinces',
         width=0.9,
-        fill_color=factor_cmap(
-            'provinces',
-            palette=Viridis256,
-            factors=provinces
-        ),
+        color=Purples[3],
         fill_alpha=0.9,
-        source=data
+        source=source
     )
 
     graph.y_range.start = 0
@@ -53,18 +59,15 @@ if __name__ == '__main__':
     graph.xgrid.grid_line_color = None
     graph.axis.minor_tick_line_color = None
     graph.outline_line_color = None
+    # graph.add_tools(hover)
 
-    # # Add Tooltips
-    hover = HoverTool()
-    hover.tooltips = """
-        <div>
-            <h3>$name</h3>
-            <div><b>Provincia</b> @provinces</div>
-            <div><b>Salarie</b> @$dates</div>
-        </div>
-    """
-    graph.add_tools(hover)
 
 #?   Show or save results
-    # show(graph)
-    save(graph)
+    def show_or_save(option):
+        if option == 1:
+            show(graph)
+        else:
+            save(graph)
+            print('_' * 3, 'Graph saved', '_' * 3,)
+
+    show_or_save(0)
