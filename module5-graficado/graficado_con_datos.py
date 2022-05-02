3988e57a92db7d3685bfc84dae4b050742ac1270
Juan Di Pasquo <juandipasquo@gmail.com>
2022-05-02 14:18:48 -0300

    Change -- The type of glyph and distribution of data

--- module5-graficado/graficado_con_datos.py
+++ module5-graficado/graficado_con_datos.py
@@ -1,15 +1,16 @@
-# Bokeh
+# Maths
+from math import pi
+#? Bokeh
 from bokeh.plotting import figure, output_file, show, save
-from bokeh.models import ColumnDataSource, Legend, LegendItem
+from bokeh.models import Legend, LegendItem
 from bokeh.models.tools import HoverTool, PanTool
+from bokeh.transform import cumsum
 from bokeh.palettes import viridis
-# Pandas
-# import pandas
-# Data
-from data import provinces, dates, data
+#? Pandas
+import pandas
 
 #   Todo
-##  Show a hover with detailed information
+##  Show legend with all the provinces
 
 
 if __name__ == '__main__':
@@ -17,61 +18,72 @@ if __name__ == '__main__':
 
 #*   CSV
     # Read in csv
-    # df = pandas.read_csv('w_mean_complete.csv')
-
-    # # Create ColumnDataSource from data frame
-    # source = ColumnDataSource(df)
+    df = pandas.read_csv('./w_mean_complete.csv', usecols=['provinces' , 'salaries'])
+    print('\n', df.info())
+    # Create ColumnDataSource from data frame
 
     output_file('women_salary.html')
 
 
+    # Group by provinces and sum up the salaries
+    total_salaries = df.groupby('provinces')['salaries'].apply(sum)
+
+    # Set provinces for the legend and color palette
+    provinces = df['provinces']
+    provinces = set(provinces)# Remove duplicates
+    provinces = sorted(provinces)# Sort provinces
+
+    # Show dataframes
+    # print(total_salaries, '\n -' * 2)
+    # print(provinces, '\n -' * 2)
+
+    # Prepare data for the graph
+    data = pandas.Series(total_salaries).reset_index(name='value').rename(columns={'index': 'province'})
+    data['angle'] = data['value']/data['value'].sum() * 2*pi
+    data['color'] = viridis(len(provinces))
+
+
 #*   Add plot
     graph = figure(
-        background_fill_color='#595260',
-        plot_height=750,
-        plot_width=1400,
+        background_fill_color='#BFAE8E',
+        plot_height=700,
+        plot_width=700,
         title_location='above',
-        title='Women salary',
+        title='Women salary \nOrdered by province',
         tools='save, reset',
-        x_axis_label='Salaries',
-        y_range=dates,
-        y_axis_label='Dates',
     )
 
 #*   Render glyph
-    graph.hbar_stack(
-        provinces,
-        y='dates',
-        height=0.9,
-        color=viridis(len(provinces)),
-        fill_alpha=0.9,
-        legend_label=provinces,
-        source=data,
-    )
+    graph.wedge(x=0, y=1, radius=0.6,
+        start_angle=cumsum('angle', include_zero=True), end_angle=cumsum('angle'),
+        line_color="white", fill_color='color', source=data)
 
 #   Plot arrangements
-    graph.y_range.range_padding = 0.07
-    graph.x_range.range_padding = 0.68
-    graph.xgrid.grid_line_color = None
-    graph.ygrid.grid_line_color = None
     graph.axis.minor_tick_line_color = None
+    graph.axis.visible = False
+    graph.grid.grid_line_color = None
     graph.outline_line_color = None
+    graph.title.background_fill_color = '#F2EFDF'
+    graph.title.text_color = '#278C5D'
+    graph.title.text_font_size = '25px'
+    graph.x_range.range_padding = 0.68
+    graph.y_range.range_padding = 0.07
 
-#   Toolbar
+#*   Annotations
+##   Toolbar
     hover = HoverTool()
     hover.tooltips = [
-        ('Provincia:', '$name'),
-        ('Fecha:', '@dates'),
-        # ('Salarios:', '$y{0,0.0}')
+        ('Provincia:', '@provinces'),
+        ('Salarios:', '@value{$0,0.00}')
     ]
     graph.add_tools(hover)
     graph.add_tools(PanTool(dimensions='width'))
     graph.toolbar.autohide = True
 
-#   Legend
-    graph.legend.location = "top_left"
-    graph.legend.click_policy="mute"
-    graph.legend.orientation = "vertical"
+##   Legend
+    # graph.legend.location = 'top_left'
+    # graph.legend.click_policy='mute'
+    # graph.legend.orientation = 'vertical'
 
 
 #?   Show or save results
